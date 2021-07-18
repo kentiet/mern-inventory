20f87b062f169f217b3dc3c564dd42aef55e1f0e
kentiet <ken.tietkien@gmail.com>
2021-07-17 23:30:32 -0700

    added the transaction button at the frontend

--- client/src/components/Comsumable/Consumable.js
+++ client/src/components/Comsumable/Consumable.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import { Button, Icon, Table } from 'semantic-ui-react'
+import { Button, Grid, Icon, Table } from 'semantic-ui-react'
 
 const Consumable = ({ id, name, description, quantity, price, vendor, deleteItem }) => {
   return (
@@ -11,23 +11,44 @@ const Consumable = ({ id, name, description, quantity, price, vendor, deleteItem
             <Table.Cell>{price}</Table.Cell>
             <Table.Cell>{vendor}</Table.Cell>
             <Table.Cell>
-                <Button basic color='violet' animated>
-                    <Link
-                        style={{ textDecorationColor: 'none', textDecoration: 'none' ,color: '#6435c9'}}
-                        to={`/items/${id}/edit`}
-                    >
-                        <Button.Content visible>Edit</Button.Content>
-                        <Button.Content hidden>
-                            <Icon name='edit outline' />
-                        </Button.Content>
-                    </Link>
-                </Button>
-                <Button basic color='red' animated onClick={() => deleteItem(id)}>
-                    <Button.Content visible>Delete</Button.Content>
-                    <Button.Content hidden>
-                            <Icon name='trash alternate outline' />
-                    </Button.Content>
-                </Button>
+                <Grid columns={3} divided>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Button basic color='violet' animated>
+                                <Link
+                                    style={{ textDecorationColor: 'none', textDecoration: 'none' ,color: '#6435c9'}}
+                                    to={`/items/${id}/edit`}
+                                >
+                                    <Button.Content visible>Edit</Button.Content>
+                                    <Button.Content hidden>
+                                        <Icon name='edit outline' />
+                                    </Button.Content>
+                                </Link>
+                            </Button>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Button basic color='yellow' animated>
+                                <Link
+                                    style={{ textDecorationColor: 'none', textDecoration: 'none' ,color: '#fbbd08'}}
+                                    to={``}
+                                >
+                                    <Button.Content visible>Check out!</Button.Content>
+                                    <Button.Content hidden>
+                                        <Icon name='opencart' />
+                                    </Button.Content>
+                                </Link>
+                            </Button>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Button basic color='red' animated onClick={() => deleteItem(id)}>
+                                <Button.Content visible>Delete</Button.Content>
+                                <Button.Content hidden>
+                                        <Icon name='trash alternate outline' />
+                                </Button.Content>
+                            </Button>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
             </Table.Cell>
         </Table.Row>
 )
