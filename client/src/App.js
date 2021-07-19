65f859102ad456c1e4cdf56ccbcf6b4b4219cd7b
kentiet <ken.tietkien@gmail.com>
2021-07-18 22:35:51 -0700

    added the asset checkout feature & fix the refresh persist state in React issue

--- client/src/App.js
+++ client/src/App.js
@@ -9,34 +9,36 @@ import {
 import AddEditAsset from './components/AddEditAsset/AddEditAsset';
 import ConsumableList from './components/ConsumableList/ConsumableList';
 import AddEditConsumable from './components/AddEditConsumable/AddEditConsumable';
-import React , {Component } from 'react';
+import React , {Component, useEffect, useState } from 'react';
 import ProtectedRoute from './components/ProtectedRoute/ProtectedRoute';
 
 import LoginForm from './components/LoginForm/LoginForm';
 import { Container } from 'semantic-ui-react';
 import TransactionForm from './components/TransactionForm/TransactionForm';
 import TransactionsList from './components/TransactionsList/TransactionsList';
+import CheckOutAsset from './components/CheckOutAsset/CheckOutAsset';
 
-class App extends Component {
+function App() {
 
-  constructor(props) {
-    super(props)
+  // constructor(props) {
+  //   super(props)
 
-    this.state = {
-      isLogin: false
-    }
-  }
+  //   this.state = {
+  //     isLogin: false
+  //   }
+  // }
+  const [isLogin, setIsLogin] = useState(localStorage.getItem('token'))
 
-  componentDidMount() {
-    const token = localStorage.getItem('token')
-    const expires = localStorage.getItem('expires')
-    if (token) {
-      this.setState({isLogin: true})
-    } 
-  }
-
-render() {
-  console.log("app.js logged", this.state.isLogin);
+  useEffect(() => { 
+    setIsLogin(localStorage.getItem('token'))
+  })
+  // componentDidMount() {
+  //   const token = localStorage.getItem('token')
+  //   const expires = localStorage.getItem('expires')
+  //   if (token) {
+  //     this.setState({isLogin: true})
+  //   } 
+  // }
   
   return (
       <Container>
@@ -44,20 +46,21 @@ render() {
         <br />
         <Switch>
           <Route path="/" exact component={LoginForm} />
-          <ProtectedRoute path="/home" exact component={Home} isAuth={this.state.isLogin}/>
-          <ProtectedRoute path="/assets" exact component={AssetList} isAuth={this.state.isLogin}/>
-          <ProtectedRoute path="/assets/create" exact component={AddEditAsset} isAuth={this.state.isLogin}/>
-          <ProtectedRoute path="/assets/:id/edit" exact component={AddEditAsset} isAuth={this.state.isLogin}/>
-          <ProtectedRoute path="/items" exact component={ConsumableList} isAuth={this.state.isLogin}/>
-          <ProtectedRoute path="/items/create" exact component={AddEditConsumable} isAuth={this.state.isLogin}/>
-          <ProtectedRoute path="/items/:id/edit" exact component={AddEditConsumable} isAuth={this.state.isLogin}/>
-          <ProtectedRoute path="/transactions/" exact component={TransactionsList} isAuth={this.state.isLogin}/>
-          <ProtectedRoute path="/transactions/:id/checkout" exact component={TransactionForm} isAuth={this.state.isLogin}/>
+          <ProtectedRoute path="/home" exact component={Home} isAuth={isLogin}/>
+          <ProtectedRoute path="/assets" exact component={AssetList} isAuth={isLogin}/>
+          <ProtectedRoute path="/assets/create" exact component={AddEditAsset} isAuth={isLogin}/>
+          <ProtectedRoute path="/assets/:id/edit" exact component={AddEditAsset} isAuth={isLogin}/>
+          <ProtectedRoute path="/assets/:id/checkout" exact component={CheckOutAsset} isAuth={isLogin}/>
+          <ProtectedRoute path="/items" exact component={ConsumableList} isAuth={isLogin}/>
+          <ProtectedRoute path="/items/create" exact component={AddEditConsumable} isAuth={isLogin}/>
+          <ProtectedRoute path="/items/:id/edit" exact component={AddEditConsumable} isAuth={isLogin}/>
+          <ProtectedRoute path="/transactions/" exact component={TransactionsList} isAuth={isLogin}/>
+          <ProtectedRoute path="/transactions/:id/checkout" exact component={TransactionForm} isAuth={isLogin}/>
+          
         </Switch>
 
       </Container>
     );
-  }
 }
 
 export default App;
--- client/src/components/AssetItem/Asset.js
+++ client/src/components/AssetItem/Asset.js
@@ -2,12 +2,64 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Asset.css'
 
-import { Table, Button, Icon } from 'semantic-ui-react'
+import { Table, Button, Icon, Grid } from 'semantic-ui-react'
 
 
 const Asset = ({ id, name, description, assetNumber, price, custodian, vendor, direction, deleteAsset }) => {
+    if (direction === 'out') { 
+        return (
+            <Table.Row disabled>
+            <Table.Cell>{name}</Table.Cell>
+            <Table.Cell>{description}</Table.Cell>
+            <Table.Cell>{assetNumber}</Table.Cell>
+            <Table.Cell>{price}</Table.Cell>
+            <Table.Cell>{custodian}</Table.Cell>
+            <Table.Cell>{vendor}</Table.Cell>
+            <Table.Cell>{direction}</Table.Cell>
+            {/* <Table.Cell>
+            <Grid columns={3} divided>
+                <Grid.Row>
+                    <Grid.Column>
+                        <Button basic color='violet' animated>
+                            <Link
+                                style={{ textDecorationColor: 'none', textDecoration: 'none' ,color: '#6435c9'}}
+                                to={`/assets/${id}/edit`}
+                            >
+                                <Button.Content visible>Edit</Button.Content>
+                                <Button.Content hidden>
+                                    <Icon name='edit outline' />
+                                </Button.Content>
+                            </Link>
+                        </Button>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Button basic color='yellow' animated>
+                            <Link
+                                style={{ textDecorationColor: 'none', textDecoration: 'none' ,color: '#fbbd08'}}
+                                to={`/assets/${id}/checkout`}
+                            >
+                                <Button.Content visible>Check out!</Button.Content>
+                                <Button.Content hidden>
+                                    <Icon name='opencart' />
+                                </Button.Content>
+                            </Link>
+                        </Button>
+                    </Grid.Column>
+                    <Grid.Column>
+                        <Button basic color='red' onClick={() => deleteAsset(id)} animated>
+                            <Button.Content visible>Delete</Button.Content>
+                            <Button.Content hidden>
+                                    <Icon name='trash alternate outline' />
+                            </Button.Content>
+                        </Button>
+                    </Grid.Column>
+                </Grid.Row>
+            </Grid>
+            </Table.Cell> */}
+        </Table.Row>
+        )
+    } else { 
     return (
-        <>
             <Table.Row>
                 <Table.Cell>{name}</Table.Cell>
                 <Table.Cell>{description}</Table.Cell>
@@ -17,27 +69,48 @@ const Asset = ({ id, name, description, assetNumber, price, custodian, vendor, d
                 <Table.Cell>{vendor}</Table.Cell>
                 <Table.Cell>{direction}</Table.Cell>
                 <Table.Cell>
-                    <Button basic color='violet' animated>
-                        <Link
-                            style={{ textDecorationColor: 'none', textDecoration: 'none' ,color: '#6435c9'}}
-                            to={`/assets/${id}/edit`}
-                        >
-                            <Button.Content visible>Edit</Button.Content>
-                            <Button.Content hidden>
-                                <Icon name='edit outline' />
-                            </Button.Content>
-                        </Link>
-                    </Button>
-                    <Button basic color='red' onClick={() => deleteAsset(id)} animated>
-                        <Button.Content visible>Delete</Button.Content>
-                        <Button.Content hidden>
-                                <Icon name='trash alternate outline' />
-                        </Button.Content>
-                    </Button>
+                <Grid columns={3} divided>
+                    <Grid.Row>
+                        <Grid.Column>
+                            <Button basic color='violet' animated>
+                                <Link
+                                    style={{ textDecorationColor: 'none', textDecoration: 'none' ,color: '#6435c9'}}
+                                    to={`/assets/${id}/edit`}
+                                >
+                                    <Button.Content visible>Edit</Button.Content>
+                                    <Button.Content hidden>
+                                        <Icon name='edit outline' />
+                                    </Button.Content>
+                                </Link>
+                            </Button>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Button basic color='yellow' animated>
+                                <Link
+                                    style={{ textDecorationColor: 'none', textDecoration: 'none' ,color: '#fbbd08'}}
+                                    to={`/assets/${id}/checkout`}
+                                >
+                                    <Button.Content visible>Check out!</Button.Content>
+                                    <Button.Content hidden>
+                                        <Icon name='opencart' />
+                                    </Button.Content>
+                                </Link>
+                            </Button>
+                        </Grid.Column>
+                        <Grid.Column>
+                            <Button basic color='red' onClick={() => deleteAsset(id)} animated>
+                                <Button.Content visible>Delete</Button.Content>
+                                <Button.Content hidden>
+                                        <Icon name='trash alternate outline' />
+                                </Button.Content>
+                            </Button>
+                        </Grid.Column>
+                    </Grid.Row>
+                </Grid>
                 </Table.Cell>
             </Table.Row>
-        </>
-    )
+        )
+    }
 }
 
 export default Asset
