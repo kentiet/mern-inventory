6e1de6c5a36a4dda167e30c1e5313bbb8e0011b1
kentiet <ken.tietkien@gmail.com>
2021-07-21 21:21:20 -0700

    refactor navbar component

--- client/src/components/HomeItem/HomeItem.js
+++ client/src/components/HomeItem/HomeItem.js
@@ -1,36 +1,38 @@
-import React, { useEffect, useState } from 'react'
-import { Link } from 'react-router-dom'
-import { Card } from 'semantic-ui-react'
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
+import { Card } from 'semantic-ui-react';
 
 const HomeItem = ({ itemName, path }) => {
-  const [pickColor, setPickColor] = useState('red')
-
-  const colors = [
-     "red","orange","yellow","olive","green","teal",
-     "blue","violet","purple","pink","brown","grey","black"  
-  ]
-
-  const randomPickColor = () => { 
-    const maxLength = colors.length
-    const picked = Math.floor(Math.random() * (maxLength - 1))
-    
-    return colors[picked]
-  }
+  const [pickColor, setPickColor] = useState('red');
   
-  useEffect(() => { 
-    let myColor = randomPickColor()
-    setPickColor(myColor)
-  }, [randomPickColor()])
+  useEffect(() => {
+    const colors = [
+      'red',
+      'orange',
+      'yellow',
+      'olive',
+      'green',
+      'teal',
+      'blue',
+      'violet',
+      'purple',
+      'pink',
+      'brown',
+      'grey',
+      'black',
+    ];
+
+    const maxLength = colors.length;
+    const picked = Math.floor(Math.random() * (maxLength - 1));
+
+    setPickColor(colors[picked]);
+  }, []);
 
   return (
     <Link to={path}>
-      <Card
-        color={ pickColor }
-        href={ path }
-        header={ itemName }
-      />
+      <Card color={pickColor} href={path} header={itemName} />
     </Link>
-  )
-}
+  );
+};
 
 export default HomeItem;
